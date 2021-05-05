50ea101e0fc70a221fea18052ec3c127cd79d46e
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-05-04 19:55:46 -0600

    App now communicating with AWS

--- App.js
+++ App.js
@@ -3,13 +3,22 @@ import {View, Text, Button, StyleSheet} from 'react-native';
 
 export default() => {
   const [count, setCount] = useState(0);
+  const [status, setStatus] = useState(0);
+  const refresh = () => {
+    fetch('https://z2v1aoq2ib.execute-api.us-west-1.amazonaws.com/dev')
+      .then(response => {return response.json();})
+      .then(json => {setStatus(json.statusCode);})
+      .catch(() => {setStatus(status + 1);})
+  }
   return (
       <View>
         <View style={styles.container}>
           <Text style={styles.text}>{count}</Text>
+          <Text style={styles.status}>Status code: {status}</Text>
         </View>
-        <Button color='red'title='Increment' onPress={() => setCount(count + 1)}/>
-        <Button color='red' title='Decrement' onPress={() => setCount(count - 1)}/>
+        <Button color='red' title='Refresh' onPress={() => refresh()}/>
+        <Button title='Increment' onPress={() => setCount(count + 1)}/>
+        <Button title='Decrement' onPress={() => setCount(count - 1)}/>
       </View>
     );
 }
@@ -17,4 +26,5 @@ export default() => {
 const styles = StyleSheet.create({
     container: {alignItems: 'center'},
     text: {fontSize: 100, color: 'red'},
+    status: {fontSize: 25, color: 'white'},
   });
